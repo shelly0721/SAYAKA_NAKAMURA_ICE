d24687c1cf2cce5b0c53992063636de54e371c07
sayaka <b082313w@gmail.com>
2022-02-04 09:46:16 -0500

    Commit

--- src/ca/sheridancollege/week2/softwarefundamentals/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/Card.java
@@ -1,4 +1,7 @@
 /*
+ * modifier: Sayaka Nakamura
+ * StudentID: 991 645 277
+ *
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.

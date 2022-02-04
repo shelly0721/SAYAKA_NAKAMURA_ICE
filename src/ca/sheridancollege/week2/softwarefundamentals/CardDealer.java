f59b4519a44efb1408d80169bb5bed7182fe2d7c
shelly0721 <98172794+shelly0721@users.noreply.github.com>
2022-02-04 09:53:50 -0500

    CardReader.java
    modified card suit from diamonds to clubs

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -17,6 +17,6 @@ public class CardDealer
     public static void main(String[] args)
     {
         Card nineClubs = new Card("Clbs", 9);
-        nineClubs.setSuit("diamonds");
+        nineClubs.setSuit("Culbs");
     }
 }
